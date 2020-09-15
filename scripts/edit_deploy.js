8a3d978e75dfd7fd7dbb0ec48c2e19bf1877837b
b1mind <brent@1mindsolutions.com>
2020-09-15 11:03:38 -0600

    🚀msg for starting deploy

--- scripts/edit_deploy.js
+++ scripts/edit_deploy.js
@@ -1,5 +1,6 @@
 const ghpages = require('gh-pages')
 //You must add your own info and repo name change < > items
+console.log('\x1b[1m\x1b[36m Starting deployment to github \x1b[0m')
 ghpages.publish(
   './dist/build/',
   {
@@ -12,5 +13,5 @@ ghpages.publish(
   },
   () => {
     console.log('\x1b[1m\x1b[33m Github Pages Deploy Complete! \x1b[0m')
-  },
+  }
 )
