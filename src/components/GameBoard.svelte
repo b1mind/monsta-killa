e6cf2aadffab49c32684e784fd34937b3cfdf363
b1mind <brent@1mindsolutions.com>
2020-09-17 00:02:14 -0600

    🚀OnBoarding UX started 📦game board to component

---
+++ src/components/GameBoard.svelte
@@ -0,0 +1,179 @@
+<script>
+  import { slide } from 'svelte/transition'
+
+  const minAtk = 4
+  const strongAtk = 7
+  const playerStr = 10
+
+  //todo make one object for gameOptions+reset?
+  let healStr = 14
+  let playerHp = 100
+  let monsterStr = 11
+  let monsterHp = 100
+  let strongAttacks = 3
+  let heals = 1
+  export let msg = `Make your move`
+  let round = 0
+  let endGame = false
+  let keyCode
+
+  //> Function junction whats the malfunction
+  function reset() {
+    endGame = false
+    playerHp = 100
+    monsterHp = 100
+    strongAttacks = 3
+    heals = 1
+    healStr = 14
+    round = 0
+    monsterStr = 11
+    msg = 'Try harder! Attack!'
+  }
+
+  //todo refactor for difficulty
+  function monsterAtk() {
+    const monsterAtkDmg = Math.ceil(Math.random() * monsterStr)
+    playerHp -= monsterAtkDmg
+    console.log(monsterAtkDmg, playerHp)
+    endTurn()
+  }
+
+  function playerAtk(atkType) {
+    let playerAtkDmg = Math.ceil(Math.random() * playerStr)
+    playerAtkDmg < minAtk ? (playerAtkDmg += minAtk) : playerAtkDmg
+
+    if (atkType === 'strong') {
+      if (strongAttacks === 0) {
+        return (msg = 'No strong Attacks left')
+      } else {
+        playerAtkDmg += strongAtk
+        strongAttacks--
+      }
+    }
+
+    monsterHp -= playerAtkDmg
+    console.log(playerAtkDmg)
+    monsterAtk()
+  }
+
+  //todo add animations to buttons and key press
+  function handleKeyDown(event) {
+    keyCode = event.keyCode
+
+    if (endGame === true) {
+      keyCode === 82 ? reset() : false
+      return
+    }
+
+    if (keyCode == 74) {
+      playerAtk()
+    } else if (keyCode === 75) {
+      playerAtk('strong')
+    } else if (keyCode === 72) {
+      healPlayer()
+    }
+  }
+
+  function endTurn() {
+    if (monsterHp <= 0) {
+      msg = 'Player Wins'
+      endRound(false)
+    } else if (playerHp <= 0) {
+      msg = 'Monster Ate Player, Try Again'
+      endRound(true)
+    } else {
+      msg = 'Make your move'
+    }
+  }
+
+  function endRound(end) {
+    if (end) {
+      return (endGame = true)
+    } else {
+      round++
+      playerHp = 100
+      monsterHp = 100
+      strongAttacks >= 9 ? false : strongAttacks++
+
+      if (round % 5 === 0) {
+        monsterStr++
+        strongAttacks >= 9 ? false : (strongAttacks += 2)
+        healStr += 10
+      }
+
+      if (round % 10 === 0) {
+        heals++
+      }
+    }
+  }
+
+  function healPlayer() {
+    const healPwr = Math.ceil(Math.random() * 10 + healStr)
+    if (heals <= 0) return (msg = 'You are out of heals')
+    playerHp += healPwr
+    heals--
+    monsterAtk()
+  }
+</script>
+
+<svelte:window on:keydown={handleKeyDown} />
+
+<div>
+  <p>{msg}</p>
+  <h2>Round: {round}</h2>
+  <h3>Monster</h3>
+  <div class="progress-bar">
+    <h3>{monsterHp} ♥</h3>
+    <span style="width: {monsterHp}%" />
+  </div>
+  <br />
+
+  <h3>Player</h3>
+  <div class="progress-bar">
+    <h3>{playerHp} ♥</h3>
+    <span style="width: {playerHp}%" />
+  </div>
+  <br />
+
+  {#if endGame}
+    <div class="retry" id="test" transition:slide={{ y: -50 }}>
+      <button on:click={reset}>Retry</button>
+    </div>
+  {:else if !endGame}
+    <div class="controls" transition:slide={{ y: -50 }}>
+      <button on:click={playerAtk}>Attack </button>
+      <button on:click={() => playerAtk('strong')}>Fire {strongAttacks}</button>
+      <button on:click={healPlayer}>Heal {heals}</button>
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  //todo make me look better
+  .progress-bar {
+    position: relative;
+    height: 30px;
+    background: #2d3647;
+    border-radius: 5px;
+    overflow: hidden;
+
+    span {
+      position: absolute;
+      left: 0;
+      height: 100%;
+      background: skyblue;
+      transition: width 0.5s ease-out;
+      z-index: 0;
+    }
+
+    h3 {
+      position: relative;
+      margin: 0;
+      padding: 5px 0;
+      display: inline-block;
+      color: white;
+      font-weight: 700;
+      z-index: 99;
+    }
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,159 +1,35 @@
 <script>
-  import { slide } from 'svelte/transition'
+  import GameBoard from '../components/GameBoard.svelte'
 
-  const minAtk = 4
-  const strongAtk = 7
-  const playerStr = 10
+  let onBoard = true
+  let gameMode = 0
 
-  //todo make one object for gameOptions+reset?
-  let healStr = 14
-  let playerHp = 100
-  let monsterStr = 11
-  let monsterHp = 100
-  let strongAttacks = 3
-  let heals = 1
-  let msg = `Make your move`
-  let round = 0
-  let endGame = false
-
-  let key
-  let keyCode
-
-  //> Function junction whats the malfunction
-  function reset() {
-    endGame = false
-    playerHp = 100
-    monsterHp = 100
-    strongAttacks = 3
-    heals = 1
-    healStr = 14
-    round = 0
-    monsterStr = 11
-    msg = 'Try harder! Attack!'
-  }
-
-  //todo refactor for difficulty
-  function monsterAtk() {
-    const monsterAtkDmg = Math.ceil(Math.random() * monsterStr)
-    playerHp -= monsterAtkDmg
-    console.log(monsterAtkDmg, playerHp)
-    endTurn()
-  }
-
-  function playerAtk(atkType) {
-    let playerAtkDmg = Math.ceil(Math.random() * playerStr)
-    playerAtkDmg < minAtk ? (playerAtkDmg += minAtk) : playerAtkDmg
-
-    if (atkType === 'strong') {
-      if (strongAttacks === 0) {
-        return (msg = 'No strong Attacks left')
-      } else {
-        playerAtkDmg += strongAtk
-        strongAttacks--
-      }
-    }
-
-    monsterHp -= playerAtkDmg
-    console.log(playerAtkDmg)
-    monsterAtk()
-  }
-
-  //todo add animations to buttons and key press
-  function handleKeyDown(event) {
-    key = event.key
-    keyCode = event.keyCode
-
-    keyCode === 82 ? reset() : false
-
-    if (endGame === true) {
-      return
-    }
-
-    if (keyCode == 74) {
-      playerAtk()
-    } else if (keyCode === 75) {
-      playerAtk('strong')
-    } else if (keyCode === 72) {
-      healPlayer()
-    }
-  }
-
-  function endTurn() {
-    if (monsterHp <= 0) {
-      msg = 'Player Wins'
-      endRound(false)
-
-      //is player dead???
-    } else if (playerHp <= 0) {
-      msg = 'Monster Ate Player, Try Again'
-      endRound(true)
-    } else {
-      msg = 'Make your move'
-    }
-  }
-
-  function endRound(end) {
-    if (end) {
-      return (endGame = true)
-    } else {
-      round++
-      playerHp = 100
-      monsterHp = 100
-      strongAttacks >= 9 ? false : strongAttacks++
-
-      if (round % 5 === 0) {
-        monsterStr++
-        strongAttacks >= 9 ? false : (strongAttacks += 2)
-        healStr += 10
-      }
-
-      if (round % 10 === 0) {
-        heals++
-      }
-    }
-  }
-
-  function healPlayer() {
-    const healPwr = Math.ceil(Math.random() * 10 + healStr)
-    if (heals <= 0) return (msg = 'You are out of heals')
-    playerHp += healPwr
-    heals--
-    monsterAtk()
+  function playGame() {
+    onBoard = false
   }
 </script>
 
-<svelte:window on:keydown={handleKeyDown} />
-
 <main>
   <h1 class="headline">Monster Hunter</h1>
-  <p>{msg}</p>
-  <h3>Round: {round}</h3>
 
-  <!-- //Todo figure best way to animate bars and health nums -->
-  <h3>Monster</h3>
-  <div class="progress-bar">
-    <h3>{monsterHp} ♥</h3>
-    <span style="width: {monsterHp}%" />
-  </div>
-  <br />
+  {#if onBoard}
+    <p>Welcome, please pick your difficulty</p>
 
-  <h3>Player</h3>
-  <div class="progress-bar">
-    <h3>{playerHp} ♥</h3>
-    <span style="width: {playerHp}%" />
-  </div>
-  <br />
-
-  {#if endGame}
-    <div class="retry" id="test" transition:slide={{ y: -50 }}>
-      <button on:click={reset}>Retry</button>
+    <div class="difficulty">
+      <label>
+        <input type="radio" bind:group={gameMode} value={1} /> Easy
+      </label>
+      <label>
+        <input type="radio" bind:group={gameMode} value={2} /> Medium
+      </label>
+      <label>
+        <input type="radio" bind:group={gameMode} value={3} /> Hard
+      </label>
     </div>
-  {:else if !endGame}
-    <div class="controls" transition:slide={{ y: -50 }}>
-      <button on:click={playerAtk}>Attack </button>
-      <button on:click={() => playerAtk('strong')}>Fire {strongAttacks}</button>
-      <button on:click={healPlayer}>Heal {heals}</button>
-    </div>
+
+    <button on:click={playGame}>Play! {gameMode}</button>
+  {:else}
+    <GameBoard />
   {/if}
 </main>
 
@@ -163,32 +39,6 @@
     margin: 0 auto;
     font-family: sans-serif;
     text-align: center;
-  }
-
-  //todo make me look better
-  .progress-bar {
-    position: relative;
-    height: 30px;
-    background: #2d3647;
-    border-radius: 5px;
-    overflow: hidden;
-    span {
-      position: absolute;
-      left: 0;
-      height: 100%;
-      background: skyblue;
-      transition: width 0.5s ease-out;
-      z-index: 0;
-    }
-    h3 {
-      position: relative;
-      margin: 0;
-      padding: 5px 0;
-      display: inline-block;
-      color: white;
-      font-weight: 700;
-      z-index: 99;
-    }
   }
 
   .headline {
