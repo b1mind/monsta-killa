000fbcd2e87e7c314a71b237c6e007946adbad75
b1mind <brent@1mindsolutions.com>
2020-09-23 02:21:52 -0600

    🧹clean up unused import/plugin

--- src/client.js
+++ src/client.js
@@ -1,8 +1,4 @@
 import * as sapper from '@sapper/app'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-
-gsap.registerPlugin(ScrollTrigger)
 
 sapper.start({
   target: document.querySelector('#sapper'),
