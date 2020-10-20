4661580d148fbf140f58322b9c52d3e444a99f06
b1mind <brent@1mindsolutions.com>
2020-10-20 02:33:18 -0600

    📃 log styles

--- src/components/Logs.svelte
+++ src/components/Logs.svelte
@@ -2,11 +2,30 @@
   import { battleLogs } from '../components/stores/gameStore'
 </script>
 
-{#each $battleLogs as battleLog}
-  <p>{battleLog.event} | {battleLog.atk}</p>
-{:else}
-  <p>Game Logs</p>
-{/each}
+<div class="logWrap">
+  {#each $battleLogs as battleLog}
+    {battleLog.event}
+
+    {battleLog.atk}
+    <br />
+  {:else}
+    <p>Game Logs</p>
+  {/each}
+</div>
 
 <style lang="scss">
+  .logWrap {
+    height: 125px;
+    padding: 0.75em 0 0 1em;
+    grid-row: 1 / span 2;
+    font-size: small;
+    text-align: left;
+    background: var(--clr-dark);
+    box-shadow: var(--shadow-inner);
+    overflow-x: hidden;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      width: 0.5em;
+    }
+  }
 </style>
